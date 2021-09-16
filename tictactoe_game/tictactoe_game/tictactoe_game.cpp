702d41e03e49e57dae725d407af8726a279c0fdc
maxmakovskiy <makovskiymax@gmail.com>
2021-09-16 22:54:38 +0300

    Standee window

--- tictactoe_game/tictactoe_game/tictactoe_game.cpp
+++ tictactoe_game/tictactoe_game/tictactoe_game.cpp
@@ -2,48 +2,39 @@
 
 #include <windows.h>
 #include <windowsx.h>
+#include <tchar.h>
 
 
 // Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-// WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+ HINSTANCE hInst;                                // current instance
+#define szWindowClass _T("WINCLASS1")            // the main window class name
+#define szTitle _T("Tictactoe Game")
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+// ENTRY POINT
 int WINAPI wWinMain(HINSTANCE hInstance,
                     HINSTANCE hPrevInstance,
                     LPWSTR    lpCmdLine,
                     int       nCmdShow)
-
-
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_TICTACTOEGAME, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
+{
     // Perform application initialization:
+    MyRegisterClass(hInstance);
     if (!InitInstance (hInstance, nCmdShow))
     {
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TICTACTOEGAME));
-
     MSG msg;
 
     // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (GetMessage(&msg, NULL, 0, 0))
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
     return (int) msg.wParam;
@@ -71,7 +62,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     winclass.hCursor        = LoadCursor(NULL, IDC_ARROW);
     winclass.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
     winclass.lpszMenuName   = NULL;
-    winclass.lpszClassName  = _T("WINCLASS1");
+    winclass.lpszClassName  = szWindowClass;
     winclass.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
 
     return RegisterClassExW(&winclass);
@@ -91,8 +82,10 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   HWND hWnd = CreateWindowW(
+       szWindowClass, szTitle, (WS_OVERLAPPEDWINDOW|WS_VISIBLE),
+       0, 0, 400, 400,
+       NULL, NULL, hInstance, NULL);
 
    if (!hWnd)
    {
@@ -117,58 +110,26 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    PAINTSTRUCT ps;
+    HDC hdc;
+
     switch (message)
     {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
+    {
+        hdc = BeginPaint(hWnd, &ps);
+
+        // redraw window
+
+        EndPaint(hWnd, &ps);
+    } break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
     default:
+        // windows proccesses other messages 
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
+
     return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
