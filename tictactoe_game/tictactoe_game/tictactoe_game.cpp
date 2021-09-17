ba536e8f340b9084e8963ed8bf78a5e49f6e3d1d
maxmakovskiy <makovskiymax@gmail.com>
2021-09-17 22:48:55 +0300

    Slicing the board

--- tictactoe_game/tictactoe_game/tictactoe_game.cpp
+++ tictactoe_game/tictactoe_game/tictactoe_game.cpp
@@ -3,17 +3,21 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <tchar.h>
+#include <math.h>
 
 
 // Global Variables:
  HINSTANCE hInst;                                // current instance
 #define szWindowClass _T("WINCLASS1")            // the main window class name
 #define szTitle _T("Tictactoe Game")
+const int CELL_SIZE = 100;
 
 // Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
+ATOM                MyRegisterClass(HINSTANCE);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL                GetGameboardRect(HWND, RECT*);
+void                DrawLine(HDC, int, int, int, int, bool);
 
 // ENTRY POINT
 int WINAPI wWinMain(HINSTANCE hInstance,
@@ -54,7 +58,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     winclass.hInstance      = hInstance;
     winclass.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
     winclass.hCursor        = LoadCursor(NULL, IDC_ARROW);
-    winclass.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    winclass.hbrBackground  = (HBRUSH)(GetStockObject(GRAY_BRUSH));
     winclass.lpszMenuName   = NULL;
     winclass.lpszClassName  = szWindowClass;
     winclass.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
@@ -88,7 +92,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     PAINTSTRUCT ps;
     HDC hdc;
-    const int CELL_SIZE = 100;
 
     switch (message)
     {
@@ -99,6 +102,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     } break;
     case WM_GETMINMAXINFO:
     {
+        // disallow window make smaller than
         MINMAXINFO* pMinMax = (MINMAXINFO*)lParam;
         pMinMax->ptMinTrackSize.x = CELL_SIZE * 5;
         pMinMax->ptMinTrackSize.y = CELL_SIZE * 5;
@@ -107,22 +111,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_PAINT:
     {
         hdc = BeginPaint(hWnd, &ps);
+        RECT rect;
         // redraw client area
-
-        // we need to calculate center of client area
-        RECT rc;
-        if (GetClientRect(hWnd, &rc))
+        if (GetGameboardRect(hWnd, &rect))
         {
-            int width = rc.right - rc.left;
-            int height = rc.bottom - rc.top;
+            // draw gameboard rectangle
+            FillRect(hdc, &rect, (HBRUSH)GetStockObject(WHITE_BRUSH));
 
-            // calculate sides of rectangular gameboard
-            int left = (width - CELL_SIZE * 3) / 2;
-            int top = (height - CELL_SIZE * 3) / 2;
-            int right = left + CELL_SIZE * 3;
-            int bottom = top + CELL_SIZE * 3;
-            
-            Rectangle(hdc, left, top, right, bottom);
+            // draw horizontal lines
+            DrawLine(hdc, rect.left, rect.top + CELL_SIZE, rect.right, rect.top + CELL_SIZE, TRUE);
+            DrawLine(hdc, rect.left, rect.top + CELL_SIZE * 2, rect.right, rect.top + CELL_SIZE * 2, TRUE);
+
+            // draw vertical lines
+            DrawLine(hdc, rect.left + CELL_SIZE, rect.top, rect.left + CELL_SIZE, rect.bottom, FALSE);
+            DrawLine(hdc, rect.left + CELL_SIZE * 2, rect.top, rect.left + CELL_SIZE * 2, rect.bottom, FALSE);
 
         }
 
@@ -138,3 +140,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     return 0;
 }
+
+BOOL GetGameboardRect(HWND hWnd, RECT *pRect)
+{
+    // we need to calculate center of client area
+    RECT rc;
+    if (GetClientRect(hWnd, &rc))
+    {
+        int width = rc.right - rc.left;
+        int height = rc.bottom - rc.top;
+
+        // calculate sides of rectangular gameboard
+        pRect->left = (width - CELL_SIZE * 3) / 2;
+        pRect->top = (height - CELL_SIZE * 3) / 2;
+        pRect->right = pRect->left + CELL_SIZE * 3;
+        pRect->bottom = pRect->top + CELL_SIZE * 3;
+        return TRUE;
+    }
+
+    SetRectEmpty(pRect);
+    return FALSE;
+}
+
+void DrawLine(HDC hdc, int startx, int starty, int endx, int endy, bool isHorizontal)
+{
+    const double SIZE_OF_PADDING = 0.03; // 3%
+    double padding = sqrt(pow((double)endx-startx, 2) + pow((double)endy-starty, 2)) * SIZE_OF_PADDING;
+
+    if (isHorizontal)
+    {
+        MoveToEx(hdc, startx + padding, starty, NULL);
+        LineTo(hdc, endx - padding, endy);
+    }
+    else
+    {
+        MoveToEx(hdc, startx, starty + padding, NULL);
+        LineTo(hdc, endx, endy - padding);
+    }
+}
+
