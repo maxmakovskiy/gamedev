b68d6d058815718c1bf4aaad4e89ca99703e69fc
maxmakovskiy <makovskiymax@gmail.com>
2021-09-21 13:49:47 +0300

    Basics of sprite bouncing

--- simple_drawing_sfml/simple_drawing_sfml/src/main.cpp
+++ simple_drawing_sfml/simple_drawing_sfml/src/main.cpp
@@ -1,9 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <stdio.h>
 
-int main() 
+
+void CalculateNewDirection(const sf::RenderWindow&, const sf::Sprite&, const sf::Vector2u&, sf::Vector2f&);
+
+int main()
 {
-	sf::RenderWindow window(sf::VideoMode(720, 480), "Application SFML");
+	sf::RenderWindow window(sf::VideoMode(720, 480), "Bouncing ball");
+
+	sf::Texture textureCyanBall;
+	sf::Sprite spriteCyanBall;
+
+	sf::Vector2u sizeCyanBall;
+	sf::Vector2f increment(0.4f, 0.4f);
+
+	const char* pathToCyanBallImage = "F:\\gamedev\\simple_drawing_sfml\\simple_drawing_sfml\\src\\ballCyan.png";
+	if (!textureCyanBall.loadFromFile(pathToCyanBallImage))
+	{
+		fprintf(stderr, "Error with load the image from path: %s", pathToCyanBallImage);
+	}
+
+	spriteCyanBall.setTexture(textureCyanBall);
+
+	// get size of cyan ball sprite
+	sizeCyanBall.x = textureCyanBall.getSize().x;
+	sizeCyanBall.y = textureCyanBall.getSize().y;
 	
+	spriteCyanBall.setOrigin(sizeCyanBall.x / 2, sizeCyanBall.y / 2);
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -15,11 +39,33 @@ int main()
 			}
 			
 		}
+	
+		CalculateNewDirection(window, spriteCyanBall, sizeCyanBall, increment);
+		spriteCyanBall.setPosition(spriteCyanBall.getPosition() + increment);
 
 		window.clear(sf::Color::Black);
+		window.draw(spriteCyanBall);
 		window.display();
 	}
 
 
 	return 0;
+}
+
+void CalculateNewDirection(const sf::RenderWindow& window, const sf::Sprite& sprite, const sf::Vector2u& size, sf::Vector2f& direction)
+{
+	if((sprite.getPosition().x + (size.x / 2) > window.getSize().x && direction.x > 0) ||
+		(sprite.getPosition().x - (size.x / 2) < 0 && direction.x < 0))
+	{
+		// Reverse the direction on X axis.
+		direction.x = -direction.x;
+	}
+
+	if((sprite.getPosition().y + (size.y / 2) > window.getSize().y && direction.y > 0) ||
+		(sprite.getPosition().y - (size.y / 2) < 0 && direction.y < 0))
+	{
+		// Reverse the direction on Y axis.
+		direction.y = -direction.y;
+	}
+
 }
