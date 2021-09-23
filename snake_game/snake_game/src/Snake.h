517944e7894bcb5e9eb5f9299bddc87d66888b91
maxmakovskiy <makovskiymax@gmail.com>
2021-09-23 21:48:07 +0300

    Updated Snake.h

--- snake_game/snake_game/src/Snake.h
+++ snake_game/snake_game/src/Snake.h
@@ -22,4 +22,47 @@ enum class Direction
 	Right
 };
 
+class Snake
+{
+public:
+	Snake(int blockSize);
+	~Snake();
+
+	// helper methods
+	// Setters:
+	void IncreaseScore();
+	void SetDirection(Direction obj);
+	// Getters:
+	Direction GetDirection() const;
+	int GetSpeed() const;
+	sf::Vector2i GetPosition() const;
+	int GetLives() const;
+	int GetScore() const;
+	bool IsLost() const;
+	
+	void Lose(); // lose control is happening here
+	void ToggleLost();
+
+	void Extend(); // grow the snake
+	void Reset();  // put snake on starting position
+
+	void Move(); // move sprites
+	void Tick(); // update method
+	void Cut(int segments); // method for cutting snake after head of snake touch own body or walls
+
+	void Render(sf::RenderWindow& window);
+
+private:
+	SnakeContainer snakeBody;	// vector of snake's segments that represents whole snake body
+	Direction currentDirection; // current direction of snake moving
+	int snakeSpeed;				// current snake speed
+	int snakeLives;				// current snake lives
+	int playerScore;			// current player score
+	bool lost;					// current state of losing 
+	int oneRectSize;			// size of one graphic rectangle that represents one Snake Segment
+	sf::RectangleShape bodyRect;// whole shape that will be using for rendering the snake
+
+	void CheckCollision();
+
+};
 
