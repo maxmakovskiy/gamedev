014979dca4506849b75970abbffef4bb7c674d26
maxmakovskiy <makovskiymax@gmail.com>
2021-10-04 12:16:49 +0300

    GameState declaration added

---
+++ state_managment_sfml_cmake/state_managment_sfml_cmake/include/GameState.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "BaseState.h"
+#include "EventManager.h"
+
+class GameState : public BaseState
+{
+public:
+	GameState(StateManager* stateManager)
+		: BaseState(stateManager)
+	{}
+
+	// callbacks for switching to the pause state and menu state
+	void MainMenu(EventDetails* details);
+	void Pause(EventDetails* details);
+	
+	void OnCreate();
+	void OnDestroy();
+
+	void Activate();
+	void Deactivate();
+
+	void Update(const sf::Time& time);
+	void Draw();
+
+private:
+	sf::Texture ballTexture;
+	sf::Sprite spriteTexture;
+	sf::Vector2f ballSpeed;
+
+};
+
