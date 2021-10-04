0b11af4dd928400904f719f59dac9824b5aaf4d5
maxmakovskiy <makovskiymax@gmail.com>
2021-10-04 13:10:11 +0300

    PausedState class declaration added

---
+++ state_managment_sfml_cmake/state_managment_sfml_cmake/include/PausedState.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "BaseState.h"
+#include "EventManager.h"
+
+class PausedState : public BaseState
+{
+public:
+	PausedState(StateManager* stateManager)
+		: BaseState(stateManager)
+	{}
+
+	void Upaused(EventDetails* details);
+
+	void OnCreate();
+	void OnDestroy();
+
+	void Activate() {}
+	void Deactivate() {}
+
+	void Update(const sf::Time& time);
+	void Draw();
+	
+private:
+	sf::Text text;
+	sf::Font font;
+	sf::RectangleShape rect;
+};
+
